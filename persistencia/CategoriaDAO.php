e495034c6ea23a2eca1702a59d2689736f82038c
DevChris <developer@asjurfinlegaltech.com>
2024-10-13 12:34:21 -0500

    feacture-logic: CategoriaDAO.php (consulta) consulta de eventos por categoria

--- persistencia/CategoriaDAO.php
+++ persistencia/CategoriaDAO.php
@@ -16,29 +16,16 @@ class CategoriaDAO{
 
     public function consultaIndividual($idCategoria) {
         return "SELECT 
-            e.idEvento, 
-            e.sitio, 
-            e.flayer, 
-            e.logo, 
-            e.edadMinima, 
-            e.nombre AS artista, 
-            e.fechaEvento, 
-            e.horaEvento, 
-            p.usuario AS proveedor, -- Cambié el alias para que coincida con el atributo
-            c.nombre AS ciudad, 
-            ca.nombre AS categoria -- Asegúrate de incluir la categoría
-        FROM 
-            evento e 
-        INNER JOIN 
-            proveedor p ON e.Proveedor = p.idProveedor 
-        INNER JOIN 
-            ciudad c ON e.ciudad_idciudad = c.idciudad 
-        INNER JOIN 
-            categoria ca ON e.categoria_idCategoria = ca.idCategoria
-
-        WHERE ca.idCategoria = $idCategoria;
-        ";
-    }
+                e.idEvento, e.sitio, e.flayer, e.logo, e.edadMinima, e.nombre AS artista, e.fechaEvento, e.horaEvento, 
+                p.usuario AS proveedor,  -- Aquí el nombre del proveedor, si lo necesitas
+                c.nombre AS ciudad, 
+                ca.nombre AS categoria
+            FROM evento e 
+            INNER JOIN proveedor p ON e.Proveedor = p.idProveedor 
+            INNER JOIN ciudad c ON e.ciudad_idciudad = c.idciudad 
+            INNER JOIN categoria ca ON e.categoria_idCategoria = ca.idCategoria
+            WHERE ca.idCategoria = $idCategoria ORDER BY e.fechaEvento";
+        }
     
 }
 
